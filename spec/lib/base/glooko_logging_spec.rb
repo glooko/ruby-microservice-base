94760c459392f6a6619cd5b0402ba45f480445e3
Josip Papež <papezjosip@gmail.com>
2022-10-14 15:15:02 +0200

    ICO-2939/JSON-logging-framework
    Wrote test for checking if correct severity is displayed.
    Moved log method to lazy variable.

--- spec/lib/base/glooko_logging_spec.rb
+++ spec/lib/base/glooko_logging_spec.rb
@@ -1,29 +1,73 @@
-# frozen_string_literal: true
-
 RSpec.describe 'GlookoLogging' do
   let(:logger_class) { Class.new { include Base::GlookoLogging } }
+  let(:severity) { 'warn' }
+  let(:test_log) do
+    logger_class.new.log(severity,
+                         {fog: 'test'},
+                         __LINE__,
+                         __method__,
+                         __FILE__)
+  end
 
   it 'logs expected output with json data' do
-    test = expect do
-      logger_class.new.log('warn', {fog: 'test'}, __LINE__, __method__,
-                           __FILE__)
-    end
-    test.to output(/fog/).to_stdout
+    expect { test_log }.to output(/fog/).to_stdout
   end
 
   it 'logs expected output with correct file name' do
-    test = expect do
-      logger_class.new.log('warn', 123, __LINE__, __method__,
-                           __FILE__)
-    end
-    test.to output(/glooko_logging_spec.rb/).to_stdout
+    expect { test_log }.to output(/glooko_logging_spec.rb/).to_stdout
   end
 
   it 'logs expected output with correct line number' do
-    test = expect do
-      logger_class.new.log('warn', 123, __LINE__, __method__,
-                           __FILE__)
+    expect { test_log }.to output(/7/).to_stdout
+  end
+
+  it 'log gets called with correct arguments' do
+    test_log do
+      expect(logger_class.new)
+        .to have_received(:log)
+        .with('warn', {fog: 'test'}, 6, :test_log, 'glooko_logging_spec.rb')
     end
-    test.to output(/24/).to_stdout
+  end
+
+  context 'when logger logs severity' do
+    context 'for debug seveirty' do
+      let(:severity) { 'debug' }
+
+      it 'logs expected severity' do
+        expect { test_log }.to output(/DEBUG/).to_stdout
+      end
+    end
+
+    context 'for error seveirty' do
+      let(:severity) { 'error' }
+
+      it 'logs expected severity' do
+        expect { test_log }.to output(/ERROR/).to_stdout
+      end
+    end
+
+    context 'for info severity' do
+      let(:severity) { 'info' }
+
+      it 'logs expected severity' do
+        expect { test_log }.to output(/INFO/).to_stdout
+      end
+    end
+
+    context 'for fatal severity' do
+      let(:severity) { 'fatal' }
+
+      it 'logs expected severity' do
+        expect { test_log }.to output(/FATAL/).to_stdout
+      end
+    end
+
+    context 'for unknown severity' do
+      let(:severity) { 'unknown' }
+
+      it 'logs expected severity' do
+        expect { test_log }.to output(/ANY/).to_stdout
+      end
+    end
   end
 end
